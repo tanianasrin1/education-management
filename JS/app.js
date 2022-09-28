2664dfd7d9c9e1714b7e76e3ce37d87e66edc6c7
tanianasrin1 <tasrineva169@gmail.com>
2022-09-28 23:25:54 +0600

    school part fixed

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,14 @@
-// alert('nbnbmbjhj')
+let valueDisplay =document.querySelectorAll(".num");
+let interval =4000;
+valueDisplay.forEach(valueDisplay =>{
+    let startValue =0;
+    let endValue =parseInt(valueDisplay.getAttribute("data-val"));
+   let duration = Math.floor(interval / endValue);
+   let counter = setInterval(function(){
+    startValue += 1 ;
+    valueDisplay.textContent = startValue
+    if(startValue == endValue ){
+        clearInterval (counter)
+    }
+   },duration)
+} )
