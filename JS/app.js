e6db5ea01791d0371eeffd740fc9eba41dbdcfa8
tanianasrin1 <tasrineva169@gmail.com>
2022-10-02 00:31:22 +0600

    active nav added

--- JS/app.js
+++ JS/app.js
@@ -38,7 +38,7 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 
-    myTimeOut = setTimeout(showSlides, 8000);
+    myTimeOut = setTimeout(showSlides, 5000);
     slideIndex++
 }
 
@@ -48,39 +48,3 @@ function currentSlide(n) {
   showSlides();
 }
 
-// card 
-let span = document.getElementsByTagName('span');
-	let product = document.getElementsByClassName('teacher_card')
-	let product_page = Math.ceil(teacher_card.length/4);
-	let l = 0;
-	let movePer = 25.34;
-	let maxMove = 203;
-	// mobile_view	
-	let mob_view = window.matchMedia("(max-width: 768px)");
-	if (mob_view.matches)
-	 {
-	 	movePer = 50.36;
-	 	maxMove = 504;
-	 }
-
-	let right_mover = ()=>{
-		l = l + movePer;
-		if (teacher_card == 1){l = 0; }
-		for(const i of teacher_card)
-		{
-			if (l > maxMove){l = l - movePer;}
-			i.style.left = '-' + l + '%';
-		}
-
-	}
-	let left_mover = ()=>{
-		l = l - movePer;
-		if (l<=0){l = 0;}
-		for(const i of teacher_card){
-			if (product_page>1){
-				i.style.left = '-' + l + '%';
-			}
-		}
-	}
-	span[1].onclick = ()=>{right_mover();}
-	span[0].onclick = ()=>{left_mover();}
